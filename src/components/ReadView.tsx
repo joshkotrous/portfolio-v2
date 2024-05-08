cab769c92bc9340111816642574f8cb8326bca3d
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-05-08 01:44:04 -0500

    updated bottom spacing

--- src/components/ReadView.tsx
+++ src/components/ReadView.tsx
@@ -32,7 +32,7 @@ const ReadView: React.FC<ReadViewProps> = ({ filepath, date }) => {
   }, []);
 
   return (
-    <div className="bg-white/40 text-white mt-5 mb-4 p-4">
+    <div className="bg-white/40 text-white mt-5 p-4">
       <Link to={"/"} className="inline-block">
         <IoIosClose className="text-6xl m-[-16px] mb-[2px] hover:scale-80 transition-all" />
       </Link>
